ac100ebcd1262b23cf340538e9e00ad6c453fefb
Usuario <Usuario@DESKTOP-SJR6E1A>
2020-09-30 17:32:29 -0300

    Paneles de ingreso y juego en interfaz

--- TaTeToro/src/interfaz/Interfaz.java
+++ TaTeToro/src/interfaz/Interfaz.java
@@ -3,11 +3,21 @@ package interfaz;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Interfaz {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+
 
 	/**
 	 * Launch the application.
@@ -29,6 +39,11 @@ public class Interfaz {
 	 * Create the application.
 	 */
 	public Interfaz() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		initialize();
 	}
 
@@ -37,44 +52,126 @@ public class Interfaz {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 340);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setBackground(UIManager.getColor("Button.background"));
 		frame.getContentPane().setLayout(null);
 		
+		
+		JPanel ingreso = new JPanel();
+		ingreso.setBounds(0, 0, 338, 269);
+		frame.getContentPane().add(ingreso);
+		ingreso.setLayout(null);
+		
+		JPanel juego = new JPanel();
+		juego.setBounds(0, 0, 350, 301);
+		frame.getContentPane().add(juego);
+		
+		juego.setLayout(null);
+		juego.setVisible(false);
+		
+		JButton btnNewButton = new JButton("Jugar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ingreso.setVisible(false);
+				juego.setVisible(true);
+			}
+		});
+		btnNewButton.setBounds(111, 174, 109, 42);
+		ingreso.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setBounds(139, 77, 135, 20);
+		ingreso.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(139, 108, 135, 20);
+		ingreso.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Jugador 1");
+		lblNewLabel.setBounds(67, 80, 72, 14);
+		ingreso.add(lblNewLabel);
+		
+		JLabel lblJugador = new JLabel("Jugador 2");
+		lblJugador.setBounds(67, 111, 72, 14);
+		ingreso.add(lblJugador);
+		
+		
+		
+		
 		JButton btn_1 = new JButton("1");
-		btn_1.setBounds(115, 47, 60, 60);
-		frame.getContentPane().add(btn_1);
+		btn_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_1.setBounds(79, 60, 55, 55);
+		juego.add(btn_1);
 		
 		JButton btn_2 = new JButton("2");
-		btn_2.setBounds(185, 47, 60, 60);
-		frame.getContentPane().add(btn_2);
+		btn_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_2.setBounds(144, 60, 55, 55);
+		juego.add(btn_2);
 		
 		JButton btn_3 = new JButton("3");
-		btn_3.setBounds(255, 47, 60, 60);
-		frame.getContentPane().add(btn_3);
+		btn_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_3.setBounds(209, 60, 55, 55);
+		juego.add(btn_3);
 		
 		JButton btn_4 = new JButton("4");
-		btn_4.setBounds(115, 118, 60, 60);
-		frame.getContentPane().add(btn_4);
+		btn_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_4.setBounds(79, 126, 55, 55);
+		juego.add(btn_4);
 		
 		JButton btn_5 = new JButton("5");
-		btn_5.setBounds(185, 118, 60, 60);
-		frame.getContentPane().add(btn_5);
+		btn_5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_5.setBounds(144, 126, 55, 55);
+		juego.add(btn_5);
 		
 		JButton btn_6 = new JButton("6");
-		btn_6.setBounds(255, 118, 60, 60);
-		frame.getContentPane().add(btn_6);
+		btn_6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_6.setBounds(209, 126, 55, 55);
+		juego.add(btn_6);
 		
 		JButton btn_7 = new JButton("7");
-		btn_7.setBounds(115, 189, 60, 60);
-		frame.getContentPane().add(btn_7);
+		btn_7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_7.setBounds(79, 192, 55, 55);
+		juego.add(btn_7);
 		
 		JButton btn_8 = new JButton("8");
-		btn_8.setBounds(185, 189, 60, 60);
-		frame.getContentPane().add(btn_8);
+		btn_8.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_8.setBounds(144, 192, 55, 55);
+		juego.add(btn_8);
 		
 		JButton btn_9 = new JButton("9");
-		btn_9.setBounds(255, 189, 60, 60);
-		frame.getContentPane().add(btn_9);
+		btn_9.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btn_9.setBounds(209, 192, 55, 55);
+		juego.add(btn_9);
+		frame.setBackground(UIManager.getColor("Button.disabledShadow"));
+		frame.setBounds(100, 100, 356, 340);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
