110cc75e7e0ebf8348dcb89d754e74b76369cfc1
FrancoBre <bregolif.fb@gmail.com>
2020-10-08 23:15:35 -0300

    Corregido pero no pasa un test

--- TaTeToro/src/juego/Jugada.java
+++ TaTeToro/src/juego/Jugada.java
@@ -5,7 +5,8 @@ public class Jugada {
 	public static boolean esGanadora(Tablero tablero, Jugador jugador) {
 		if(esGanadoraHorizontal(tablero, jugador) ||
 				esGanadoraVertical(tablero, jugador) ||
-				esGanadoraDiagonal(tablero, jugador))
+				esGanadoraDiagonal(tablero, jugador) ||
+				esGanadoraToroidal(tablero, jugador))
 			return true;
 		return false;
 	}
@@ -59,34 +60,40 @@ public class Jugada {
 		
 		return ret;
 	}
+	
+	private static boolean esGanadoraToroidal(Tablero tablero, Jugador jugador) {
+		return (esGanadoraDiagonal(matrizToroidal1(tablero), jugador) ||
+				esGanadoraDiagonal(matrizToroidal2(tablero), jugador) ||
+				esGanadoraDiagonal(matrizToroidal3(tablero), jugador) ||
+				esGanadoraDiagonal(matrizToroidal4(tablero), jugador));
+	}
 
 	//Primera columna pasa a ser la ultima
-	public static Tablero matrizToroidal1(Tablero tablero) {
+	private static Tablero matrizToroidal1(Tablero tablero) {
 		Tablero tableroToro=new Tablero();
 		int[][] matriz=new int[3][3];
 		
-		for(int i=0; i<tableroToro.tamanio(); i++)  		//filas
-			for(int j=1; j<tableroToro.tamanio(); j++) 		//columnas
-
+		for(int i=0; i<tableroToro.tamanio(); i++)  		
+			for(int j=1; j<tableroToro.tamanio(); j++) 		
 				matriz[i][j]=tablero.getTablero()[i][j-1];
 		
-		for(int h=0; h<tableroToro.tamanio(); h++) matriz[h][2]=tablero.getTablero()[h][0];
+		for(int h=0; h<tableroToro.tamanio(); h++)
+			matriz[h][tableroToro.tamanio()-1]=tablero.getTablero()[h][0];
 		
 		tableroToro.setTablero(matriz);
 		return tableroToro;
 	}
 	
 	//Ultima columna pasa a ser la primera
-	public static Tablero matrizToroidal2(Tablero tablero) {
+	private static Tablero matrizToroidal2(Tablero tablero) {
 		Tablero tableroToro=new Tablero();
 		int[][] matriz=new int[3][3];
 		
-		//La ultima columna pasa a ser la primera
-		for(int h=0; h<tableroToro.tamanio(); h++) matriz[h][0]=tablero.getTablero()[h][tableroToro.tamanio()-1];
+		for(int h=0; h<tableroToro.tamanio(); h++)
+			matriz[h][0]=tablero.getTablero()[h][tableroToro.tamanio()-1];
 		
-		//Las columnas 0 y 1 de tablero pasan a ser la 1 y 2 de matriz
-		for(int i=0; i<tableroToro.tamanio(); i++)  		//filas
-			for(int j=1; j<tableroToro.tamanio(); j++) 		//columnas
+		for(int i=0; i<tableroToro.tamanio(); i++)  		
+			for(int j=1; j<tableroToro.tamanio(); j++) 		
 				matriz[i][j]=tablero.getTablero()[i][j-1];
 		
 		tableroToro.setTablero(matriz);
@@ -94,79 +101,34 @@ public class Jugada {
 	}
 	
 	//La primera fila pasa a ser la ultima
-	public static Tablero matrizToroidal3(Tablero tablero) {
+	private static Tablero matrizToroidal3(Tablero tablero) {
 		Tablero tableroToro=new Tablero();
 		int[][] matriz=new int[3][3];
 		
-		for(int h=0; h<tableroToro.tamanio(); h++) matriz[tableroToro.tamanio()-1][h]=tablero.getTablero()[0][h];
+		for(int h=0; h<tableroToro.tamanio(); h++)
+			matriz[tableroToro.tamanio()-1][h]=tablero.getTablero()[0][h];
 		
-		//las filas 1 y 2 del tablero pasan a ser las filas 0 y 1 de la matriz
-		for(int i=0; i<tableroToro.tamanio()-1; i++) {  		//filas
-			for(int j=0; j<tableroToro.tamanio(); j++) { 		//columnas
-				System.out.println("i: "+i+" j: "+j);
+		for(int i=0; i<tableroToro.tamanio()-1; i++) 	
+			for(int j=0; j<tableroToro.tamanio(); j++)  		
 				matriz[i][j]=tablero.getTablero()[i+1][j];
-			}
-		}
-		
-		System.out.println();
-		
+
 		tableroToro.setTablero(matriz);
 		return tableroToro;
 	}
 	
 	//La ultima fila pasa a ser la primera
-		public static Tablero matrizToroidal4(Tablero tablero) {
-			Tablero tableroToro=new Tablero();
-			int[][] matriz=new int[3][3];
+	private static Tablero matrizToroidal4(Tablero tablero) {
+		Tablero tableroToro=new Tablero();
+		int[][] matriz=new int[3][3];
 			
-			for(int h=0; h<tableroToro.tamanio(); h++) matriz[0][h]=tablero.getTablero()[tableroToro.tamanio()-1][h];
-			
-			//las filas 0 y 1 del tablero pasan a ser las filas 1 y 2 de la matriz
-			for(int i=0; i<tableroToro.tamanio()-1; i++) {  		//filas
-				for(int j=0; j<tableroToro.tamanio(); j++) { 		//columnas
-					System.out.println("i: "+i+" j: "+j);
-					matriz[i+1][j]=tablero.getTablero()[i][j];
-				}
-			}
-			
-			System.out.println();
-			
-			tableroToro.setTablero(matriz);
-			return tableroToro;
-		}
-	
-	public static void main(String[] args) {
-		Tablero tablero=new Tablero();
-		Jugador jugador=new Jugador(1);
-		tablero.setJugada(jugador, 0, 0);
-		tablero.setJugada(jugador, 2, 2);
-		tablero.setJugada(jugador, 2, 0);
+		for(int h=0; h<tableroToro.tamanio(); h++) 
+			matriz[0][h]=tablero.getTablero()[tableroToro.tamanio()-1][h];
 		
-		for(int i=0; i<3; i++) {			
-			for(int j=0; j<3; j++) {	
-				System.out.print(tablero.getTablero()[i][j]);
-			}
-			System.out.println();
-		}
-		System.out.println();
-		Tablero matrizToro=Jugada.matrizToroidal4(tablero);
-		
-		for(int i=0; i<3; i++) {			
-			for(int j=0; j<3; j++) {	
-				System.out.print(matrizToro.getTablero()[i][j]);
-			}
-			System.out.println();
-		}
-
-		
+		for(int i=0; i<tableroToro.tamanio()-1; i++) 	
+			for(int j=0; j<tableroToro.tamanio(); j++) 		
+				matriz[i+1][j]=tablero.getTablero()[i][j];
+			
+		tableroToro.setTablero(matriz);
+		return tableroToro;
 	}
-	
 }
-
-
-
-
-
-
-
-
